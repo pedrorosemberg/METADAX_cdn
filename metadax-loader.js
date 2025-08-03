88e892b17f9cf5c295f52b0aeff92a5208426592
PEDRO ROSEMBERG <dev@pedrorosemberg.com>
2025-08-02 23:23:44 -0300

    update

--- metadax-loader.js
+++ metadax-loader.js
@@ -81,7 +81,7 @@
                 this.loadFont();
 
                 // Atualizar logo se fornecida, usando a padrão se não houver
-                this.updateLogo(this.config.logoUrl || '/assets/images/loader.png');
+                this.updateLogo(this.config.logoUrl || '/assets/images/metadax_b.png');
 
                 // Mostrar loader
                 this.show();
